f3432554bcb333f3352ae2b6f814c617c52b0d26
Albert <zan15051@gmail.com>
2022-06-03 21:27:48 +0700

    that was the best try

--- CarRent/Views/UserWindow.xaml.cs
+++ CarRent/Views/UserWindow.xaml.cs
@@ -94,13 +94,33 @@ namespace CarRent.Views
             //    _carPictureRowDefinition.Height = new GridLength(CarList.ActualWidth, GridUnitType.Pixel);
             //ChangeDTemplateRowHeight();
         }
+        private Border _border;
         private void MainBorder_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            var border = (sender as Border);
-            if (border.Height != GlobalGridLeftColumn.Width.Value/2)
-            {
-                border.Height = GlobalGridLeftColumn.Width.Value/2;
-            }
+            _border = (sender as Border);
+            ChangeBorderHeight();
+        }
+        private void ChangeBorderHeight()
+        {
+            if (_border == null) return;
+            if (_border.Height != GlobalGridLeftColumn.Width.Value / 1.79)
+                _border.Height = GlobalGridLeftColumn.Width.Value / 1.79;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ChangeBorderHeight();
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            //if (_border == null) return;
+
+            //if (UserWindowName.WindowState == WindowState.Minimized)
+            //{
+            //    _border.Height = 200;
+            //}
+            ChangeBorderHeight();
         }
     }
 }
